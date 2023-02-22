5f52acb3b6509f3e8b699453fbe0897eb163b14a
chiuzon <40052481+cron1155@users.noreply.github.com>
2023-02-22 15:14:35 +0200

    Button styling and make the modal background darker

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -1,18 +1,29 @@
 import { MaxTries } from "../constants"
+import { useModal } from "./Modal"
 import WordTry from "./WordTry"
 
 
 function HelpModal({ chosenCity }) {
-    return (<><p>Guess the city in {MaxTries} tries.</p>
+    const [_, setModal] = useModal()
+
+    return (<div><p>Guess what Romanian city is in {MaxTries} tries.</p>
         <p>Each guess must be a valid {chosenCity.length} letter city.</p>
-        <h4>Examples</h4>
+        <h4>Example</h4>
 
         <WordTry word={"DOLJ"} length={"DOLJ".length} checkWord={true} targetWord={"CLUJ"} />
 
         <p><b>J</b> is in the correct spot</p>
         <p><b>L</b> is in the word but the wrong spot</p>
         <p><b>D & O</b> is not in the word in any spot</p>
-    </>)
+
+        <button onClick={() => {
+            setModal({
+                enabled: false,
+                title: "",
+                children: undefined
+            })
+        }} className="green">PLAY NOW</button>
+    </div>)
 }
 
 export default HelpModal
